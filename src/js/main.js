45336348a122999dcb9cff95bb2577c38ee0f591
Danil Dmitriev <danil.dmitriev.2014@inbox.ru>
2024-10-07 11:51:51 +0300

    feat: add burger menu

--- src/js/main.js
+++ src/js/main.js
@@ -1,79 +1,8 @@
-class Slider {
-  currentSlideIndex = 0;
-  timerId = null;
-  intervalMs = 7000e3;
-
-  constructor(backgroundId, slidesId, paginationCirclesId) {
-    this.background = document.getElementById(backgroundId);
-    this.slides = document.getElementById(slidesId);
-    this.paginationCircles = document.getElementById(paginationCirclesId);
-  }
-
-  init() {
-    this.background.addEventListener("click", () => this.changeSlide());
-    this.paginationCircles.addEventListener("click", (event) => event.stopPropagation());
-    this.addPagination();
-    this.setAutoPlay();
-  }
-
-  setAutoPlay() {
-    this.timerId = setTimeout(this.changeSlide.bind(this), this.intervalMs);
-  }
-
-  stopAutoPlay() {
-    clearTimeout(this.timerId);
-  }
-
-  changeSlide(index) {
-    this.stopAutoPlay();
-    this.hideSlide();
-    this.removeActivePaginationClass();
-    this.changeSlideIndex(index);
-    this.showSlide();
-    this.addActivePaginationClass();
-    this.setAutoPlay();
-  }
-
-  changeSlideIndex(index) {
-    if (index) {
-      this.currentSlideIndex = index;
-    } else if (this.currentSlideIndex < this.slides.children.length - 1) {
-      this.currentSlideIndex++;
-    } else {
-      this.currentSlideIndex = 0;
-    }
-  }
-
-  showSlide() {
-    this.slides.children[this.currentSlideIndex].classList.add("show");
-  }
-
-  hideSlide() {
-    this.slides.children[this.currentSlideIndex].classList.remove("show");
-  }
-
-  createPaginationCircle() {
-    this.paginationCircles.appendChild(document.createElement("li"));
-  }
-
-  addPagination() {
-    Array.from(this.slides.children).forEach(this.createPaginationCircle.bind(this));
-    Array.from(this.paginationCircles.children).forEach((circle, index) => {
-      circle.addEventListener("click", () => this.changeSlide(index));
-    });
-
-    this.paginationCircles.children[0].classList.add("active");
-  }
-
-  addActivePaginationClass() {
-    this.paginationCircles.children[this.currentSlideIndex].classList.add("active");
-  }
-
-  removeActivePaginationClass() {
-    this.paginationCircles.children[this.currentSlideIndex].classList.remove("active");
-  }
-}
+import { Slider } from "./slider.js";
+import { ModalWindow } from "./window.js";
 
 const slider = new Slider("main_section", "slides", "pagination_circles");
+const modalWindow = new ModalWindow("menu", "burger");
 
 slider.init();
+modalWindow.init();
---
+++ src/js/window.js
@@ -0,0 +1,34 @@
+class ModalWindow {
+  TABLET_RESOLUTION = 768;
+
+  constructor(menuId, burgerId) {
+    this.burger = document.getElementById(burgerId);
+    this.menu = document.getElementById(menuId);
+  }
+
+  init() {
+    if (window.innerWidth <= this.TABLET_RESOLUTION) {
+      this.burger.addEventListener("click", () => this.open());
+      this.menu.addEventListener("click", () => this.close());
+      Array.from(this.menu.children).forEach((el) => el.addEventListener("click", (event) => event.stopPropagation()));
+    } else {
+      console.warn("Burger modal window has not been initialized");
+    }
+  }
+
+  open() {
+    this.menu.classList.remove("hidden");
+    this.menu.classList.add("opened");
+  }
+
+  close() {
+    this.menu.classList.add("closing");
+
+    setTimeout(() => {
+      this.menu.classList.add("hidden");
+      this.menu.classList.remove(...["opened", "closing"]);
+    }, 500);
+  }
+}
+
+export { ModalWindow };
