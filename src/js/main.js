c85b91ccae4875814b73a6a8ee9cdc83c7748d0b
Danil Dmitriev <danil.dmitriev.2014@inbox.ru>
2024-10-08 19:06:10 +0300

    feat: add swiper

--- src/js/main.js
+++ src/js/main.js
@@ -3,6 +3,26 @@ import { ModalWindow } from "./window.js";
 
 const slider = new Slider("main_section", "slides", "pagination_circles");
 const modalWindow = new ModalWindow("menu", "burger");
+const swiperFull = new Swiper(".swiper-full", {
+  autoplay: {
+    delay: 7000,
+  },
+  slidesPerView: "auto",
+  centeredSlides: true,
+  spaceBetween: 32,
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    renderBullet: function (index, className) {
+      return '<span class="' + className + '">' + (index + 1) + "</span>";
+    },
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
 
 slider.init();
 modalWindow.init();
